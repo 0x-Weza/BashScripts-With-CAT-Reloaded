99957256c634efbb929bc38836d6a5b5e8ef6cec
moataz1223 <157649899+moataz1223@users.noreply.github.com>
2024-01-25 14:52:56 -0500

    Add files via upload

---
+++ task2.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+echo "I will now back up your home directory, $HOME"
+
+location=$(pwd)
+timestamp=$(date +"%Y%m%d%H%M%S")
+echo "I will create save the backup file in $location"
+
+tar -cf "$loction/my_backup_$timestamp.tar" $HOME
+
+echo "Backup completed successfully"
